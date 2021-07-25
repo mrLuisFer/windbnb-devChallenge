58b904311e466a5f170df382c4bfc6d815848b6d
mrLuisFer <mrluisfeer@gmail.com>
2021-07-25 15:17:50 -0500

    feat: test lang scss or type scss

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,13 @@
   import Header from './components/Header/Header.svelte'
 </script>
 
-<main>
+<main class="container">
   <Header />
   <h1>Hello!</h1>
 </main>
 
-<style>
+<style type="text/scss" lang="scss">
+  .container {
+    display: block;
+  }
 </style>
