f0334800b95cd6813d1d0a9d58c528f60e435261
mrLuisFer <mrluisfeer@gmail.com>
2021-07-25 19:58:42 -0500

    feat: adding some code and test if everything works

--- src/components/CardsLayout/Card.svelte
+++ src/components/CardsLayout/Card.svelte
@@ -17,8 +17,25 @@
 </script>
 
 <div class="card">
-  <img src="{photo}" alt="{title}" title="{title}" />
+  <img
+    class="card-img"
+    src="{photo}"
+    alt="{title}"
+    title="{title}"
+    loading="lazy" />
+  <div class="card-details">
+    {#if superHost}
+      <p>super host</p>
+    {/if}
+  </div>
 </div>
 
 <style type="text/scss" lang="scss">
+  .card {
+    &-img {
+      width: 350px;
+      height: 238px;
+      border-radius: var(--border-radius);
+    }
+  }
 </style>
