3a6b0f9e566b6dd9c8996672baaa3a2ac3c8f420
mrLuisFer <mrluisfeer@gmail.com>
2022-02-04 13:06:03 -0600

    remove layout component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,49 @@
 <script>
-  import Layout from './layout/Layout.svelte'
+  import { setContext } from 'svelte';
+  import Header from './components/Header.svelte';
+  import Searchbar from './components/Searchbar.svelte';
+  import Content from './components/Content.svelte';
+  import SearchMenu from './components/SearchMenu/SearchMenu.svelte';
+
+  let showMenu;
+  $: showMenu = true;
+
+  setContext('showMenu', {
+    value: showMenu
+  });
 </script>
 
 <main class="container">
-  <Layout />
+  <SearchMenu showMenu="{showMenu}" />
+  <div class="layout">
+    <Header />
+    <div class="searchbar-toHide">
+      <Searchbar />
+    </div>
+    <Content />
+  </div>
 </main>
 
 <style type="text/scss" lang="scss">
   .container {
     display: block;
+  }
+
+  .searchbar-toHide {
+    @media (min-width: 1024px) {
+      display: none;
+    }
+  }
+
+  .layout {
+    @media (min-width: 768px) {
+      padding: var(--tablet-padding);
+    }
+
+    @media (min-width: 1024px) {
+      max-width: 1250px;
+      position: relative;
+      margin: 0 auto;
+    }
   }
 </style>
